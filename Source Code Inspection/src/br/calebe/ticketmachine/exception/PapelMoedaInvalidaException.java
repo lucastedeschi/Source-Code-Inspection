0ca3f943b237321291d9d98ce7a1ce228efa2fe6
lucastedeschi <lucastedeschi@DESKTOP-HCEBIUG>
2016-03-27 10:43:35 -0300

    Correção de comissão na classe SaldoInsuficienteException

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -5,5 +5,8 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class PapelMoedaInvalidaException extends Exception {
-    
+
+    PapelMoedaInvalidaException(String message) {
+        super(message);
+    }
 }
